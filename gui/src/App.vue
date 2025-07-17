a8a9a3370f24116fca55b154cfd0e5f919acfa6e
Nikolai Dorofeev - d0rich <contact@d0rich.me>
2025-07-17 07:48:45 +0400

    Configure lib for meta tags

--- gui/src/App.vue
+++ gui/src/App.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { useHead } from '@unhead/vue'
+
+useHead({
+  title: 'Exactpro Swift Hackathon 2025'
+})
 </script>
 
 <template>
