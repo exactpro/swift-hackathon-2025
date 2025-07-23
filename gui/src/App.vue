a451e75fdae9eee710f69b6a68a01ce82e7e2016
Nikolai Dorofeev - d0rich <contact@d0rich.me>
2025-07-23 17:45:05 +0400

    Root button on the bottom

--- gui/src/App.vue
+++ gui/src/App.vue
@@ -4,16 +4,20 @@ import config from '../config'
 </script>
 
 <template>
-  <div>
-    <div class="bg-base-300 p-2">
-      <RouterLink to="/" class="btn btn-secondary btn-xs">
-        <span class="font-bold">Root Navigation</span>
-      </RouterLink>
-      <span class="text-sm" v-if="config.useMock">
-        You are using GUI with mock data. Pay attention, that data is syncronized only within the same browser tab.
-      </span>
-    </div>
-    <RouterView />
+  <div class="min-h-screen contain-layout grid grid-rows-[1fr_auto]">
+    <main class="min-w-0">
+      <RouterView />
+    </main>
+    <footer class="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+      <aside>
+        <RouterLink to="/" class="btn btn-secondary btn-xs">
+          <span class="font-bold">Root Navigation</span>
+        </RouterLink>
+        <span class="text-sm" v-if="config.useMock">
+          You are using GUI with mock data. Pay attention, that data is syncronized only within the same browser tab.
+        </span>
+      </aside>
+    </footer>
   </div>
 </template>
 
