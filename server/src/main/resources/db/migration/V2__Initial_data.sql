3b1b735296c276499e536e23ba01882736b49173
Sergey Raevsky <sergey.raevsky@exactprosystems.com>
2025-08-04 20:37:15 +0400

    New currencies for DB

--- server/src/main/resources/db/migration/V2__Initial_data.sql
+++ server/src/main/resources/db/migration/V2__Initial_data.sql
@@ -1,10 +1,14 @@
 INSERT INTO CurrencyCode (code) VALUES
     ('USD'),
     ('EUR'),
-    ('USDC');
+    ('USDC'),
+    ('USDT'),
+    ('EURC');
 
 INSERT INTO Token (currencyCode, address) VALUES
-    ('USDC', '345cA3e014Aaf5dcA488057592ee47305D9B3e10');
+    ('USDC', '345cA3e014Aaf5dcA488057592ee47305D9B3e10'),
+    ('USDT', 'f25186B5081Ff5cE73482AD761DB0eB0d25abfBF'),
+    ('EURC', 'F12b5dd4EAD5F743C6BaA640B0216200e89B60Da');
 
 WITH new_client AS (
     INSERT INTO Client (fullName)
@@ -18,9 +22,8 @@ FROM new_client nc,
          ('GB33BUKB20201555555551', 'USD', 5000.00),
          ('GB33BUKB20201555555552', 'EUR', 2500.00),
          ('GB33BUKB20201555555553', 'USDC', 10000.00),
-         ('GB33BUKB20201555555554', 'USD', 5000.00),
-         ('GB33BUKB20201555555555', 'EUR', 2500.00),
-         ('GB33BUKB20201555555556', 'USDC', 10000.00)
+         ('GB33BUKB20201555555554', 'USDT', 4000.00),
+         ('GB33BUKB20201555555555', 'EURC', 30000.00)
      ) AS accounts(iban, currencyCode, balance);
 
 WITH new_client AS (
@@ -35,9 +38,8 @@ FROM new_client nc,
          ('GE60NB0000000123456781', 'USD', 5000.00),
          ('GE60NB0000000123456782', 'EUR', 2500.00),
          ('GE60NB0000000123456783', 'USDC', 10000.00),
-         ('GE60NB0000000123456784', 'USD', 5000.00),
-         ('GE60NB0000000123456785', 'EUR', 2500.00),
-         ('GE60NB0000000123456786', 'USDC', 10000.00)
+         ('GE60NB0000000123456784', 'USDT', 4000.00),
+         ('GE60NB0000000123456785', 'EURC', 3000.00)
      ) AS accounts(iban, currencyCode, balance);
 
 INSERT INTO ConversionRate (baseCurrency, targetCurrency, rate) VALUES
@@ -45,8 +47,22 @@ INSERT INTO ConversionRate (baseCurrency, targetCurrency, rate) VALUES
 ('EUR', 'USD', 1.14),
 ('USD', 'USDC', 1.00),
 ('USDC', 'USD', 1.00),
+('USD', 'USDT', 1.00),
+('USDT', 'USD', 1.00),
+('USD', 'EURC', 0.88),
+('EURC', 'USD', 1.14),
 ('EUR', 'USDC', 1.14),
-('USDC', 'EUR', 0.88);
+('USDC', 'EUR', 0.88),
+('EUR', 'USDT', 1.14),
+('USDT', 'EUR', 0.88),
+('EUR', 'EURC', 1.00),
+('EURC', 'EUR', 1.00),
+('USDC', 'USDT', 1.00),
+('USDT', 'USDC', 1.00),
+('USDC', 'EURC', 0.88),
+('EURC', 'USDC', 1.14),
+('USDT', 'EURC', 0.88),
+('EURC', 'USDT', 1.14);
 
 INSERT INTO BankETHAddress (bic, ethAddress) VALUES
 ('TESTUKLLXXX', '0xf17f52151EbEF6C7334FAD080c5704D77216b732'),
